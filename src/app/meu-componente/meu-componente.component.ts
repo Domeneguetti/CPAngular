094bee4ddcfe185f20a084ad330f1f2d1b1a2c2e
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-04 21:07:28 -0300

    Finalizacao do CheckPoint 01 Angular

---
+++ src/app/meu-componente/meu-componente.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-meu-componente',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './meu-componente.component.html',
+  styleUrl: './meu-componente.component.css'
+})
+export class MeuComponenteComponent {
+  pessoa = {
+    nome: 'Alexandre',
+    interesse: ['Carros', 'Futebol', 'Viagens']
+  }
+
+}
---
+++ tipos.ts
@@ -0,0 +1,8 @@
+let numero : number
+let texto : String
+let ativo : Boolean
+let arrayNumeros : number[]
+let pessoa : {nome: String, idade: number} = {
+  nome: "Alexandre",
+  idade: 26
+}
